fc58701db2194921f6936cb4039780eda2afb98d
zhixunt <zhixunt@cmu.edu>
2016-12-19 21:25:04 -0500

    Test the mysterious baseurl

---
+++ images/gen-svg.sh
@@ -0,0 +1,6 @@
+pdflatex sample-proof.tex
+pdf2svg sample-proof.pdf sample-proof.svg
+rm sample-proof.aux
+rm sample-proof.log
+rm sample-proof.pdf
+
---
+++ install-nokogiri.sh
@@ -0,0 +1,6 @@
+gem install nokogiri -- \
+    --use-system-libraries \
+    --with-xml2-lib=/usr/local/Cellar/libxml2/2.9.4/lib \
+    --with-xml2-include=/usr/local/Cellar/libxml2/2.9.4/include/libxml2
+bundle install
+
